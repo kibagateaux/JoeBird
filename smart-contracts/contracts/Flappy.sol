056cd401d2beee34e5a1bd0bd9cb40f39d819764
ssircar <swarnima.sircar@gmail.com>
2018-08-09 00:33:16 -0400

    changes to Flappy.sol

--- smart-contracts/contracts/Flappy.sol
+++ smart-contracts/contracts/Flappy.sol
@@ -1,36 +1,29 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.2;
 //pragma experimental ABIEncoderV2;
 
 contract Flappy {
-    /* Define variable owner of the type address */
 
-
-
-    uint private highScore = 0;
+  // =====================================================================
+  // VARIABLES
+  // =====================================================================
+    uint256 private highScore = 0;
     address private highScoreAddress;
-    uint private payout;
-    uint64[] public topTen;
+    uint256 private payout;
+    uint256[] public topTen;
 
     address owner;
     bytes32 name;
 
-    constructor(bytes32 _name) public {
-        // State variables are accessed via their name
-        // and not via e.g. this.owner. This also applies
-        // to functions and especially in the constructors,
-        // you can only call them like that ("internally"),
-        // because the contract itself does not exist yet.
+    // =====================================================================
+    // CONSTRUCTOR
+    // =====================================================================
+
+    /**
+    @param _name Bytes32 address of owner
+    */
+    constructor (bytes32 _name) public {
         owner = msg.sender;
-        // We do an explicit type conversion from `address`
-        // to `TokenCreator` and assume that the type of
-        // the calling contract is TokenCreator, there is
-        // no real way to check that.
-
         name = _name;
-    }
-
-
-    function() payable public {
     }
 
     struct player{
@@ -40,15 +33,19 @@ contract Flappy {
 
     player[] leaderboard;
 
+    // =====================================================================
+    // FLAPPY
+    // =====================================================================
 
+    /**
+    @notice when someone plays, they send eth to the smart contract that is added to the pot
+    when they are finished playing, their score and address get sent to this contract
+    their score and address is added to the ranking list, and removed if not in top 10
+    if the person just acheived the highscore, they instantly get half the pot
+    when this happens, 10% of the pot is split between the top 10 on the ranking board
+    */
 
-    //when someone plays, they send eth to the smart contract that is added to the pot
-    //when they are finished playing, their score and address get sent to this contract
-    //their score and address is added to the ranking list, and removed if not in top 10
-    //if the person just acheived the highscore, they instantly get half the pot
-    //when this happens, 10% of the pot is split between the top 10 on the ranking board
-
-    function checkScore(address playerAddress, uint64 playerScore) public returns(uint) {
+    function checkScore(address playerAddress, uint256 playerScore) public returns(uint) {
         //checks if score is in the top 10, adds it to top 10 if so
         checkInTopTen(playerAddress, playerScore);
         //checks if score is the highest, if so disburse give 50% of pot
@@ -56,21 +53,17 @@ contract Flappy {
         //pay 1% of pot to each address in top 10
         //payDividendsToTopTen();
         return playerScore;
-
-
-
     }
 
-    function getTopTen(uint index) public returns (uint64) {
+    function getTopTen(uint index) public view returns (uint256) {
         return topTen[index];
     }
 
     //only check the score of the address if it has been confirmed to pay to play the game
-    function checkIfPaid() public{
+    /* function checkIfPaid() public{
+    } */
 
-    }
-
-    function checkHighScore(address playerAddress, uint64 playerScore) public payable{
+    function checkHighScore(address playerAddress, uint256 playerScore) public payable{
         if (playerScore > highScore) {
            //set old high score to new high score
             highScore = playerScore;
@@ -84,7 +77,7 @@ contract Flappy {
         }
     }
 
-    function checkInTopTen(address playerAddress, uint64 playerScore) public {
+    function checkInTopTen(address playerAddress, uint256 playerScore) public {
         //create new struct with player's info
         player memory newPlayer = player(playerAddress, playerScore);
         //if top ten list isn't full yet, add player to top 10
@@ -116,9 +109,9 @@ contract Flappy {
 
 
 //quicksort function
-    function sort_array(uint64[] arr_) public returns (uint64 [] ){
+    function sort_array(uint256[] arr_) public returns (uint256 [] ){
         uint256 l = arr_.length;
-        uint64[] memory arr = new uint64[] (l);
+        uint256[] memory arr = new uint256[] (l);
 
         for(uint i=0;i<l;i++)
         {
@@ -131,7 +124,7 @@ contract Flappy {
             {
                 if(arr[i]<arr[j])
                 {
-                    uint64 temp= arr[j];
+                    uint256 temp= arr[j];
                     arr[j]=arr[i];
                     arr[i] = temp;
 
@@ -143,5 +136,10 @@ contract Flappy {
     return arr;
 }
 
+  // =====================================================================
+  // FALLBACK
+  // =====================================================================
+
+  function() public payable {}
 
 }
